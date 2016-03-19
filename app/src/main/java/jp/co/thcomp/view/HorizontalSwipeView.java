c6d9448949f841a81d2d17ff33f4e6c3304ce44a
辰口寿志 <h.tatsuguchi.1234@gmail.com>
2016-03-19 23:56:19 +0900

    add HorizontalSwipeView

---
+++ app/src/main/java/jp/co/thcomp/view/HorizontalSwipeView.java
@@ -0,0 +1,264 @@
+package jp.co.thcomp.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
+
+public class HorizontalSwipeView extends FrameLayout{
+    public static interface OnHiddenLayoutStatusChangeListener{
+        public void onChangeHiddenLayoutStatus(HorizontalSwipeView view, HiddenLayoutStatus status);
+    }
+
+    private static final long SwipeIntervalMS = 100;
+
+    public static enum HiddenLayoutStatus {
+        Open(10000),
+        Clocse(0);
+
+        int pos;
+        HiddenLayoutStatus(int toPos){
+            pos = toPos;
+        }
+
+        public int position(){
+            return pos;
+        }
+    }
+    private HorizontalScrollView mRootLayout;
+    private FrameLayout mMainContentLayout;
+    private FrameLayout mHiddenContentLayout;
+    private HiddenLayoutStatus mStatus= HiddenLayoutStatus.Clocse;
+    private long mDownTimeMS = 0;
+    private OnHiddenLayoutStatusChangeListener mHiddenLayoutStatusChangeListener;
+
+    public HorizontalSwipeView(Context context) {
+        super(context);
+        init();
+    }
+
+    public HorizontalSwipeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public HorizontalSwipeView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void setOnHiddenLayoutStatusChangeListener(OnHiddenLayoutStatusChangeListener listener){
+        mHiddenLayoutStatusChangeListener = listener;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+
+        // change main content layout
+        ViewGroup.LayoutParams params = mMainContentLayout.getLayoutParams();
+        if(params.width < 0) {
+            ViewParent parent = mMainContentLayout.getParent();
+            if (parent instanceof ViewGroup) {
+                ViewGroup parentViewGroup = (ViewGroup) parent;
+                params.width = r - l;
+
+                parentViewGroup.updateViewLayout(mMainContentLayout, params);
+            }
+        }
+    }
+
+    private void init(){
+        Context context = getContext();
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mRootLayout = (HorizontalScrollView)inflater.inflate(R.layout.evl_horizontal_swipeview_layout, this, false);
+        mRootLayout.setOnTouchListener(mScrollTouchListener);
+        super.addView(mRootLayout);
+
+        mMainContentLayout = (FrameLayout)findViewById(R.id.Evl_MainContent);
+        mHiddenContentLayout = (FrameLayout)findViewById(R.id.Evl_HiddenContent);
+    }
+
+    @Override
+    public void addView(View child) {
+        if(child.equals(mRootLayout)){
+            super.addView(child);
+        }else {
+            if(mMainContentLayout.getChildCount() == 0){
+                mMainContentLayout.addView(child);
+            }else{
+                mHiddenContentLayout.addView(child);
+            }
+        }
+    }
+
+    @Override
+    public void addView(View child, int index) {
+        if(child.equals(mRootLayout)){
+            super.addView(child, index);
+        }else {
+            if(mMainContentLayout.getChildCount() == 0){
+                mMainContentLayout.addView(child, index);
+            }else{
+                mHiddenContentLayout.addView(child, index);
+            }
+        }
+    }
+
+    @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        if(child.equals(mRootLayout)){
+            super.addView(child, index, params);
+        }else {
+            if(mMainContentLayout.getChildCount() == 0){
+                mMainContentLayout.addView(child, index, params);
+            }else{
+                mHiddenContentLayout.addView(child, index, params);
+            }
+        }
+    }
+
+    @Override
+    public void addView(View child, ViewGroup.LayoutParams params) {
+        if(child.equals(mRootLayout)){
+            super.addView(child, params);
+        }else {
+            if(mMainContentLayout.getChildCount() == 0){
+                mMainContentLayout.addView(child, params);
+            }else{
+                mHiddenContentLayout.addView(child, params);
+            }
+        }
+    }
+
+    @Override
+    public void addView(View child, int width, int height) {
+        if(child.equals(mRootLayout)){
+            super.addView(child, width, height);
+        }else {
+            if(mMainContentLayout.getChildCount() == 0){
+                mMainContentLayout.addView(child, width, height);
+            }else{
+                mHiddenContentLayout.addView(child, width, height);
+            }
+        }
+    }
+
+    @Override
+    protected boolean addViewInLayout(View child, int index, ViewGroup.LayoutParams params) {
+        if(child.equals(mRootLayout)){
+            return super.addViewInLayout(child, index, params);
+        }else {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @Override
+    protected boolean addViewInLayout(View child, int index, ViewGroup.LayoutParams params, boolean preventRequestLayout) {
+        if(child.equals(mRootLayout)){
+            return super.addViewInLayout(child, index, params, preventRequestLayout);
+        }else {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @Override
+    public void removeAllViews() {
+        mMainContentLayout.removeAllViews();
+        mHiddenContentLayout.removeAllViews();
+    }
+
+    @Override
+    public void removeAllViewsInLayout() {
+        mMainContentLayout.removeAllViewsInLayout();
+        mHiddenContentLayout.removeAllViewsInLayout();
+    }
+
+    @Override
+    public void removeViewAt(int index) {
+        if(mMainContentLayout.getChildCount() > index){
+            mMainContentLayout.removeViewAt(index);
+        }else{
+            mHiddenContentLayout.removeViewAt(index - mMainContentLayout.getChildCount());
+        }
+    }
+
+    @Override
+    public void removeViewInLayout(View view) {
+        mMainContentLayout.removeViewInLayout(view);
+        mHiddenContentLayout.removeViewInLayout(view);
+    }
+
+    @Override
+    public void removeViews(int start, int count) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeViewsInLayout(int start, int count) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void reuseLayout(){
+        requestLayout();
+    }
+
+    public void swipe(HiddenLayoutStatus nextStatus, boolean needAnimation){
+        if(!mStatus.equals(nextStatus)){
+            mStatus = nextStatus;
+            if(needAnimation) {
+                mRootLayout.smoothScrollTo(nextStatus.position(), 0);
+            }else{
+                mRootLayout.scrollTo(nextStatus.position(), 0);
+            }
+
+            if(mHiddenLayoutStatusChangeListener != null){
+                mHiddenLayoutStatusChangeListener.onChangeHiddenLayoutStatus(this, nextStatus);
+            }
+        }
+    }
+
+    private OnTouchListener mScrollTouchListener = new OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int action = event.getAction();
+            boolean ret = false;
+
+            switch(action){
+                case MotionEvent.ACTION_DOWN:
+                    mDownTimeMS = System.currentTimeMillis();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    if(mDownTimeMS == 0){
+                        mDownTimeMS = System.currentTimeMillis();
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                    if(System.currentTimeMillis() - mDownTimeMS > SwipeIntervalMS){
+                        // scroll
+                        if(mStatus.equals(HiddenLayoutStatus.Clocse)){
+                            swipe(HiddenLayoutStatus.Open, true);
+                        }else{
+                            swipe(HiddenLayoutStatus.Clocse, true);
+                        }
+                    }else{
+                        // return to source position
+                        swipe(mStatus, true);
+                    }
+                    ret = true;
+                    mDownTimeMS = 0;
+                    break;
+                case MotionEvent.ACTION_CANCEL:
+                    mDownTimeMS = 0;
+                    break;
+            }
+
+            return ret;
+        }
+    };
+}
---
+++ example/src/main/java/jp/co/thcomp/testapi/TestHorizontalSwipeViewActivity.java
@@ -0,0 +1,83 @@
+package jp.co.thcomp.testapi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import jp.co.thcomp.example.R;
+import jp.co.thcomp.view.HorizontalSwipeView;
+
+public class TestHorizontalSwipeViewActivity extends Activity {
+	private static final String Passphrase = "0";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		ListView contentListView = new ListView(this);
+		contentListView.setAdapter(new ListItemAdapter());
+		setContentView(contentListView);
+	}
+
+	private class ListItemAdapter extends BaseAdapter implements HorizontalSwipeView.OnHiddenLayoutStatusChangeListener {
+		private ItemData[] itemDataArray = null;
+
+		public ListItemAdapter(){
+			itemDataArray = new ItemData[getCount()];
+			for(int i=0, size=itemDataArray.length; i<size; i++){
+				itemDataArray[i] = new ItemData();
+			}
+		}
+
+		@Override
+		public int getCount() {
+			return 100;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return itemDataArray[position];
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			HorizontalSwipeView rootView = null;
+			ItemData itemData = (ItemData)getItem(position);
+
+			if(convertView == null){
+				convertView = View.inflate(TestHorizontalSwipeViewActivity.this, R.layout.test_horizontalswipeview_item, null);
+				rootView = (HorizontalSwipeView)convertView;
+				rootView.setTag(itemData);
+				rootView.setOnHiddenLayoutStatusChangeListener(this);
+			} else{
+				rootView = (HorizontalSwipeView)convertView;
+				rootView.setTag(itemData);
+
+				rootView.swipe(itemData.status, false);
+
+				// need to call "reuseLayout"
+				rootView.reuseLayout();
+			}
+
+			return convertView;
+		}
+
+		@Override
+		public void onChangeHiddenLayoutStatus(HorizontalSwipeView view, HorizontalSwipeView.HiddenLayoutStatus status) {
+			ItemData itemData = (ItemData)view.getTag();
+			itemData.status = status;
+		}
+	};
+
+	private static class ItemData{
+		public HorizontalSwipeView.HiddenLayoutStatus status = HorizontalSwipeView.HiddenLayoutStatus.Clocse;
+	}
+}
